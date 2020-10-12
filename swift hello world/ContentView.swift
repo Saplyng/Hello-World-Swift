7524546e730d7c526ea3a86c334799ce1df1e0a9
Jeremy Stein <saplyng@gmail.com>
2020-10-12 00:34:43 -0500

    Update ContentView.swift
    bibbity bobbity boop?

--- swift hello world/ContentView.swift
+++ swift hello world/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowing = false
+    
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("What's a skeletons favorite snack?")
             .padding()
+        
+        Button("what?"){
+            self.isShowing.toggle()
+        }.padding()
+        
+        .sheet(isPresented: $isShowing){
+            Text("Spare Ribs")
+        }
+    
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
