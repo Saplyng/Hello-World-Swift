a66c2a0883755bdeb90ebe5220f5dc57e65cd1b1
Jeremy Stein <saplyng@gmail.com>
2020-10-12 00:05:14 -0500

    Initial Commit

---
+++ swift hello world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift hello world
+//
+//  Created by student on 10/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
